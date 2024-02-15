2f4794356edcaf8764f6b3020f8943a38c9d18bc
qmaliha02 <144977322+qmaliha02@users.noreply.github.com>
2024-02-15 05:58:57 -0500

    menu functional but still finnicky

---
+++ Assets/Scripts/settingmenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class settingmenu : MonoBehaviour
+{
+   //Quit Game
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player has quit the game.");
+
+    }
+}

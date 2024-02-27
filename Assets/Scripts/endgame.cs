796dd156c3bab70edb8745423fff207789c726ad
qmaliha02 <144977322+qmaliha02@users.noreply.github.com>
2024-02-27 02:58:02 -0500

    audio fully works, decided end game to be a panel instead of scene

---
+++ Assets/Scripts/endgame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endgame : MonoBehaviour
+{
+    // Load scene
+   public  void  mmenu()
+    {
+        SceneManager.LoadScene("main_menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

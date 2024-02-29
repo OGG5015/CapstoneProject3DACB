e2d148207ff451054db5e21970eea31770a9516d
Olivia Gibbs <livgibbs378@icloud.com>
2024-02-29 16:30:09 -0500

    Myan added changes in person to the masterv2 branch directly - her changes included the sound scripts required for the sound effects

--- Assets/Controllers/DragAndDrop.cs
+++ Assets/Controllers/DragAndDrop.cs
@@ -12,6 +12,7 @@ public class DragAndDrop : MonoBehaviour
     Vector3 mousePosition;
     bool isDragging = false;
     Vector3 PrevPos;
+    public SFXPlaying dj;
 
     private Vector3 GetMousePos()
     {
@@ -22,6 +23,7 @@ public class DragAndDrop : MonoBehaviour
     {
         mousePosition = Input.mousePosition - GetMousePos();
         isDragging = true;
+        dj.PlayPickup();
     }
 
     private void OnMouseDrag()
@@ -43,6 +45,7 @@ public class DragAndDrop : MonoBehaviour
         SnapToHexOrUnitBench();
 
         isDragging = false;
+        dj.PlayDrop();
     }
 
     private void SnapToHexOrUnitBench()
